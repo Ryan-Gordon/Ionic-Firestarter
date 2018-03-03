b6e8df40fa9699d980424e4d57003822e9279346
Ryan Gordon <ryangordon210@gmail.com>
2018-03-03 18:44:53 +0000

    Front end for home page + Google Auth call

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import firebase from 'firebase';
+import {AuthProvider} from '../../providers/auth/auth';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  userProfile: any = null;
+  icons:string = 'log-in'; //string to keep track of which icon is showing
+  constructor(public navCtrl: NavController, public authProvider:AuthProvider) {
+    //Set up a listener for the when the AuthState changes (Login/Logout) and perform some action.
+    firebase.auth().onAuthStateChanged( user => {
+      if (user){
+        this.userProfile = user;
+      } else { 
+          this.userProfile = null;
+      }
+    });
   }
+
+    googleLogin(){
+      this.authProvider.googleLogin()
+        .then(res => {
+          //We have successfully logged in
+          //Do something with the response 
+        })
+    }
+
+  
 
 }
