7006e8a5d268e5b3751335985b77eca8d9d1c5d3
Ryan Gordon <ryangordon210@gmail.com>
2018-03-03 18:37:57 +0000

    Added example enviroment file + renamed app id

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,15 @@ import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { AuthProvider } from '../providers/auth/auth';
 
+import { GooglePlus } from '@ionic-native/google-plus';
+import {HttpModule} from '@angular/http';
+
+import { environment } from '../enviroments/enviroment';
+
+import { AngularFireModule } from 'angularfire2';
+export const firebaseConfig = environment.firebaseConfig;
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -15,7 +24,10 @@ import { AuthProvider } from '../providers/auth/auth';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    HttpModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -26,7 +38,9 @@ import { AuthProvider } from '../providers/auth/auth';
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    AuthProvider
+    AuthProvider,
+    GooglePlus,
+    HttpModule
   ]
 })
 export class AppModule {}
---
+++ src/enviroments/example.enviroment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    clientID: '<yourwebclientID>.apps.googleusercontent.com',
+    firebaseConfig: {
+        apiKey: 'APIKEY',
+        authDomain: 'DEV-APP.firebaseapp.com',
+        databaseURL: 'https://DEV-APP.firebaseio.com',
+        projectId: 'DEV-APP',
+        storageBucket: 'DEV-APP.appspot.com',
+        messagingSenderId: '123456789'
+    }
+};
